bd2faa80d6b90aa0983a28b13b7d903d2640d1ad
Odnostorontseva Evgenia <jennyodnost@mail.ru>
2025-10-26 19:45:17 +0700

    made two buttons

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,12 +1,38 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import "./App.css";
 import Reader from "./Reader";
+import SignIn from "./Sign In";
+
+function HomePage() {
+    const navigate = useNavigate();
+
+    return (
+        <div className="App">
+            <button
+                className="open-book-btn"
+                onClick={() => navigate("/reader")}
+            >
+                ХОЧУ ЧИТАТЬ!!!
+            </button>
+
+            <button className="sing-up-btn"
+                    onClick={() => navigate("/sign-in")}>
+                Sing IN
+            </button>
+        </div>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <Reader/>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/reader" element={<Reader />} />
+                <Route path="/sign-in" element={<SignIn />}></Route>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ Frontend/src/Sign In.js
@@ -0,0 +1,10 @@
+const SignIn = () => {
+
+    return (
+        <div>
+            <h1>тут будет форма, скоро, обещаю, честно</h1>
+        </div>
+    )
+}
+
+export default SignIn
