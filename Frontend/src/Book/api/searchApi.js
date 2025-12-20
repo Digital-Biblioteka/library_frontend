8fed267dce21f868f12fe41742ffd4a220260f2a
YuliaVyaznikova <y.vyaznikova@g.nsu.ru>
2025-12-21 01:16:57 +0700

    connect with real search

--- Frontend/src/Book/api/searchApi.js
+++ Frontend/src/Book/api/searchApi.js
@@ -1,28 +1,15 @@
 const API_BASE = "http://localhost:8080/api/search/books";
 
-// export async function searchBook(query) {
-//     const res = await fetch ( `${API_BASE}`, {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json"
-//         },
-//         body: query
-//     });
-//
-//     console.log(query)
-//
-//     if (!res.ok) {
-//         const msg = await res.text();
-//         throw new Error(`Ошибка поиска книг: ${msg}`);
-//     }
-//
-//     return await res.json()
-// }
-
 export async function searchBook(query) {
     const res = await fetch ( `${API_BASE}`, {
-        method: "GET",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: query
     });
+
+    console.log(query)
 
     if (!res.ok) {
         const msg = await res.text();
@@ -31,3 +18,16 @@ export async function searchBook(query) {
 
     return await res.json()
 }
+
+// export async function searchBook(query) {
+//     const res = await fetch ( `${API_BASE}`, {
+//         method: "GET",
+//     });
+
+//     if (!res.ok) {
+//         const msg = await res.text();
+//         throw new Error(`Ошибка поиска книг: ${msg}`);
+//     }
+
+//     return await res.json()
+// }
